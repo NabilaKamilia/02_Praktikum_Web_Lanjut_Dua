eb1f07986b17e76ea7f95dbb4b5d4350364f4ea2
= <=>
2021-02-21 19:53:02 +0700

    Praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Nabila Kamilia 1941720158';
+       }
+}
---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+        return 'Selamat Datang';
+       }
+    
+       public function about() {
+        return 'Nabila Kamilia 1941720158';
+       }
+    
+       public function articles($id) {
+        return 'Halaman Artikel dengan id' .$id;
+       }
+    
+}
